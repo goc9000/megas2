7b39130c90f77bbc514354f4df51704db6e66d8b
Cristian Dinu <goc9000@gmail.com>
2012-06-23 23:07:08 +0300

    Added basic register access commands to ENC28J60 stub

--- src/enc28j60.cpp
+++ src/enc28j60.cpp
@@ -10,6 +10,42 @@
 
 using namespace std;
 
+#define E28J_REVISION_ID              0x05
+
+#define OPCODE_READ_CONTROL_REG       0
+#define OPCODE_WRITE_CONTROL_REG      2
+#define OPCODE_BIT_FIELD_SET          4
+#define OPCODE_BIT_FIELD_CLEAR        5
+
+// Registers
+#define REG_ECON1                     0x1f
+#define REG_EREVID                    0xd2
+
+// Register bits
+
+#define STATE_RECEIVING_COMMAND       0
+#define STATE_RECEIVING_COMMAND_ARG   1
+#define STATE_PRE_RESPONDING          2
+#define STATE_RESPONDING              3
+
+static bool is_common_reg(uint8_t reg)
+{
+    return (reg & 0x1f) >= 0x1b;
+}
+
+static bool is_mac_reg(uint8_t reg)
+{
+    return
+        ((reg >= 0x40) && (reg <= 0x4d)) ||
+        ((reg >= 0x60) && (reg <= 0x65)) ||
+        (reg == 0x6A);
+}
+
+static bool is_mii_reg(uint8_t reg)
+{
+    return (reg >= 0x51) && (reg <= 0x59);
+}
+
 Enc28J60::Enc28J60()
 {
     this->spi_selected = false;
@@ -23,15 +59,17 @@ sim_time_t Enc28J60::nextEventTime()
 
 void Enc28J60::reset()
 {
-    printf("ENC28J reset!\n");
+    memset(this->regs, 0, E28J_REGS_COUNT);
+    memset(this->phy_regs, 0, E28J_PHY_REGS_COUNT);
+    memset(this->eth_buffer, 0, E28J_ETH_BUFFER_SIZE);
+    
+    this->regs[REG_EREVID] = E28J_REVISION_ID;
 }
 
 void Enc28J60::spiSlaveSelect(bool select)
 {
-    printf("ENC28J select <- %d\n", select);
-    
-    if (!this->spi_selected && select) {
-    }
+    if (!this->spi_selected && select)
+        this->state = STATE_RECEIVING_COMMAND;
     
     this->spi_selected = select;
 }
@@ -48,7 +86,117 @@ bool Enc28J60::spiReceiveData(uint8_t &data)
 
 uint8_t Enc28J60::_handleSpiData(uint8_t data)
 {
-    printf("ENC28J gets: %02x\n", data);
+    switch (this->state) {
+        case STATE_RECEIVING_COMMAND:
+            return this->_handleCommandStart(data);
+        case STATE_RECEIVING_COMMAND_ARG:
+            return this->_handleCommandArg(data);
+        case STATE_PRE_RESPONDING:
+            this->state = STATE_RESPONDING;
+            return 0xff;
+        case STATE_RESPONDING:
+            this->state = STATE_RECEIVING_COMMAND;
+            return this->response_byte;
+    }
     
     return 0xff;
+}
+
+uint8_t Enc28J60::_handleCommandStart(uint8_t data)
+{
+    uint8_t opcode = (data >> 5) & 0x07;
+    
+    switch (opcode) {
+        case OPCODE_READ_CONTROL_REG:
+            return this->_execReadCtrlReg(data & 0x1f);
+        case OPCODE_WRITE_CONTROL_REG:
+        case OPCODE_BIT_FIELD_CLEAR:
+        case OPCODE_BIT_FIELD_SET:
+            this->cmd_byte = data;
+            this->state = STATE_RECEIVING_COMMAND_ARG;
+            return 0xff;
+    }
+    
+    fail("Invalid command byte received by ENC28J60: %02x", data);
+    
+    return 0xff;
+}
+
+uint8_t Enc28J60::_handleCommandArg(uint8_t data)
+{
+    uint8_t opcode = (this->cmd_byte >> 5) & 0x07;
+
+    switch (opcode) {
+        case OPCODE_WRITE_CONTROL_REG:
+            return this->_execWriteCtrlReg(this->cmd_byte & 0x1f, data);
+        case OPCODE_BIT_FIELD_CLEAR:
+            return this->_execBitFieldClear(this->cmd_byte & 0x1f, data);
+        case OPCODE_BIT_FIELD_SET:
+            return this->_execBitFieldSet(this->cmd_byte & 0x1f, data);
+    }
+    
+    return 0xff;
+}
+
+uint8_t Enc28J60::_execReadCtrlReg(uint8_t reg)
+{
+    reg = this->_mapRegister(reg);
+    
+    this->response_byte = this->regs[reg];
+    this->state = (is_mac_reg(reg) || is_mii_reg(reg)) ? STATE_PRE_RESPONDING : STATE_RESPONDING; 
+    
+    return 0xff;
+}
+
+uint8_t Enc28J60::_execWriteCtrlReg(uint8_t reg, uint8_t data)
+{
+    reg = this->_mapRegister(reg);
+    
+    // TODO: intercept this
+    this->regs[reg] = data;
+    
+    this->state = STATE_RECEIVING_COMMAND;
+    
+    return 0xff;
+}
+
+uint8_t Enc28J60::_execBitFieldClear(uint8_t reg, uint8_t data)
+{
+    reg = this->_mapRegister(reg);
+    
+    if (is_mac_reg(reg) || is_mii_reg(reg))
+        fail("Attempted Bit Field Clear on MAC/MII register %02x", reg);
+    
+    // TODO: intercept this
+    this->regs[reg] &= ~data;
+    
+    this->state = STATE_RECEIVING_COMMAND;
+    
+    return 0xff;
+}
+
+uint8_t Enc28J60::_execBitFieldSet(uint8_t reg, uint8_t data)
+{
+    reg = this->_mapRegister(reg);
+    
+    if (is_mac_reg(reg) || is_mii_reg(reg))
+        fail("Attempted Bit Field Set on MAC/MII register %02x", reg);
+    
+    // TODO: intercept this
+    this->regs[reg] |= data;
+    
+    this->state = STATE_RECEIVING_COMMAND;
+    
+    return 0xff;
+}
+
+uint8_t Enc28J60::_mapRegister(uint8_t reg)
+{
+    if (reg > 0x1f)
+        fail("ENC28J60: Tried to map already mapped register!");
+    
+    if (is_common_reg(reg))
+        return reg;
+    
+    return ((this->regs[REG_ECON1] & 0x03) << 5) + reg;
 }
--- src/enc28j60.h
+++ src/enc28j60.h
@@ -6,6 +6,10 @@
 #include "spi_device.h"
 #include "device.h"
 
+#define E28J_REGS_COUNT         0x80
+#define E28J_PHY_REGS_COUNT     0x20
+#define E28J_ETH_BUFFER_SIZE    0x2000
+
 class Enc28J60 : public Device, public SpiDevice {
 public:
     Enc28J60();
@@ -14,9 +18,26 @@ public:
     void spiSlaveSelect(bool select);
     bool spiReceiveData(uint8_t &data);
 private:
+    uint8_t regs[E28J_REGS_COUNT];
+    uint8_t phy_regs[E28J_PHY_REGS_COUNT];
+    uint8_t eth_buffer[E28J_ETH_BUFFER_SIZE];
+
     bool spi_selected;
     
+    int state;
+    uint8_t cmd_byte;
+    uint8_t response_byte;
+    
     uint8_t _handleSpiData(uint8_t data);
+    uint8_t _handleCommandStart(uint8_t data);
+    uint8_t _handleCommandArg(uint8_t data);
+    
+    uint8_t _execReadCtrlReg(uint8_t reg);
+    uint8_t _execWriteCtrlReg(uint8_t reg, uint8_t data);
+    uint8_t _execBitFieldClear(uint8_t reg, uint8_t data);
+    uint8_t _execBitFieldSet(uint8_t reg, uint8_t data);
+    
+    uint8_t _mapRegister(uint8_t reg);
 };
 
 #endif
